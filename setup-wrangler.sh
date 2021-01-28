228758a423b244a722c5244e5f480be259b34996
Brady Joslin <brady@bradyjoslin.com>
2021-01-28 13:47:20 -0600

    support wrangler version input

--- setup-wrangler.sh
+++ setup-wrangler.sh
@@ -2,9 +2,20 @@
 
 set -e
 
+install_wrangler() {
+    # If a Wrangler version is detected as input
+    if [ -z "$WRANGLER_VERSION" ]
+    then
+        npm i @cloudflare/wrangler -g
+    else
+        npm i "@cloudflare/wrangler@$WRANGLER_VERSION" -g
+    fi  
+}
+
+
 if [ "$RUNNER_OS" == "Windows" ]; then
-  npm i @cloudflare/wrangler -g
+    install_wrangler
 else
-  npm i @cloudflare/wrangler -g
-  echo "~/.npm-global/bin" >> $GITHUB_PATH
+    install_wrangler
+    echo "~/.npm-global/bin" >> $GITHUB_PATH
 fi
